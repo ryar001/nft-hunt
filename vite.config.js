995b2031c569f1c8f0244103c1fb41ebb80463f2
ryar001 <57259642+ryar001@users.noreply.github.com>
2022-10-17 21:21:38 +0800

    multi page

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,27 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
+import {resolve} from "path"
 
 // https://vitejs.dev/config/
+const root = resolve(__dirname,'src') // like python Path(), create a path that resolve to src
+const outDir=resolve(__dirname,'dist')
 export default defineConfig({
+  root,
   plugins: [react()],
+  // https://vitejs.dev/config/build-options.html#build-rollupoptions
   build:{
-    target: 'esnext'
+    target: 'esnext',
+    outDir, // the output directory for our build, defualt is a folder dist
+    // tell the vite that ok to empty folder of outDir, default is true
+    // warning appear if the root directory has been change  hence need to re apply true
+    emptyOutDir: true,
+    // https://rollupjs.org/guide/en/#input 
+    rollupOptions: {
+      input: {
+        main: resolve(root, 'index.html'), // set main path to src/index.html
+        about: resolve(root, 'about', 'index.html'), //set the about page to src/about/index.hmtl
+        }
+    }
   },
   test:{ // setting for the npm test, using Vitest
     "import.meta.vitest":"undefined", // make sure our inline testing is not shown in final build
